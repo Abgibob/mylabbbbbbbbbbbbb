89659808238a091a89bd3ebc235efb0c392fcd78
Bob <Bob@mail.ru>
2025-09-24 10:18:23 +0300

    popan

--- pop.cs
+++ pop.cs
@@ -1,16 +1,1 @@
-using System; 
-
-namespace HelloWorldApp { 
-    
-    class Geeks { 
-        
-        static void Main(string[] args) { 
-            
-            // statement printing Hello World! 
-            Console.WriteLine("Hello Oleg"); 
-            
-            // To prevent the screen from running and closing quickly 
-            Console.ReadKey(); 
-        } 
-    } 
-}
+yujyutfwfwfw
