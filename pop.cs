6213500aee5cab40dedd16ff36139a8a6cd118ed
Bob <Bob@mail.ru>
2025-09-24 10:15:16 +0300

    DINOZAVR

--- pop.cs
+++ pop.cs
@@ -7,7 +7,7 @@ namespace HelloWorldApp {
         static void Main(string[] args) { 
             
             // statement printing Hello World! 
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("Hello Oleg"); 
             
             // To prevent the screen from running and closing quickly 
             Console.ReadKey(); 
